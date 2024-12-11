ed35cedfd59838296f840ead700ac54b59f05a27
Emir Zaza <e.yasarzaza@gmail.com>
2024-12-12 02:37:06 +0300

    ADD started to login page

--- App.js
+++ App.js
@@ -1,11 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput,Pressable } from 'react-native';
 
 export default function App() {
+
+const [name, setname] = useState("")
+const [surname, setSurname] = useState("")
+const [result, setResult] = useState("")
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Text>--------welcome {result} --------</Text>
+
+      <TextInput
+      placeholder='Enter your name..'
+      style={styles.input}
+      onChangeText={setname}
+      
+      />
+      <TextInput
+      placeholder='Enter your surname..'
+      style={styles.input}
+      onChangeText={setSurname}
+      />
+      <TextInput
+      placeholder='Enter your password..'
+      style={styles.input}
+      
+      />
+      <TextInput
+      placeholder='Enter your phone number..'
+      style={styles.input}
+      keyboardType='numeric'
+      />
+      <TextInput
+      placeholder='Enter your email..'
+      style={styles.input}
+     
+      />
+
+      <Pressable style={({pressed})=>[{
+        backgroundColor: pressed ? "gray" : "lightblue"
+      },
+      styles.Buton]}
+      onPress={()=> setResult(name+surname)}>
+        
+        <Text style={styles.butontext} >BUTTON</Text>
+      </Pressable>
     </View>
   );
 }
@@ -17,4 +59,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    borderWidth:1,
+    width: '80%',
+    height:50,
+    borderRadius:10,
+    textAlign: 'center',
+    marginVertical:10,
+  },
+  Buton:{
+    borderWidth:1,
+    width: '80%',
+    height:50,
+    borderRadius:10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical:10,
+    
+  },
+  butontext:{
+    color:"white",
+    fontWeight: "bold",
+  }
 });
